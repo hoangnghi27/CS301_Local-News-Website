dc8ebfdb2754e5b30e699bbd30a2b44a0cccee6f
hoangnghi27 <natnghil0911@gmail.com>
2024-05-25 18:14:36 +0700

    update about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,18 +1,30 @@
+import { NavLink } from 'react-router-dom';
 import "../styles/About.scss";
+import { MailOutlined } from "@ant-design/icons";
+
 export default function About() {
   return (
     <div className="container">
-    <div className="description">
-  <h2 className="pt-3">About Page</h2>
-  
-  <text className="description">
-    We are developers who have been working on building a website for everyone to read news from official and verified local sources. 
-<br/><br/>Our team:<br/><br/>
+      <div className="description">
+        <h2 className="pt-3">About Page</h2>
+        <p className="des-head">
+  <div className="des-body">We are developers who have been working on building a website for everyone to read news from official and verified local sources.</div>
+<div className="des-body-members"><br/>Our team:<br/><br/></div>
+<div className="members">
+  <text>
 <li>
-2159017 - Tao Cam Xuong <br/></li>
+Product Owner: Nguyen Dinh Anh Hao<br/><br/></li>
+<li>Project Manager: Le Nguyen Hoang Nghi<br/><br/></li>
 <li>
-1859014 - Nguyen Dinh Anh Hao<br/></li>
-<li>2159027 - Le Nguyen Hoang Nghi<br/><br/></li>
-  </text></div></div>
+Team member: Tao Cam Xuong<br/><br/></li></text>
+</div>
+<div className="contact">
+  <div className='contact-head'>
+    <text>Contact us:<br/></text>
+  </div>
+<div className='contact-body'><NavLink><MailOutlined/></NavLink><text>Email: localnews2024@gmail.com</text></div>
+</div></p>
+      </div>
+    </div>
   );
 }
